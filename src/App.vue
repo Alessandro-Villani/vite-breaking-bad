33edbac368f56d6b80905145ddf1cc4094f8ead9
Alessandro-Villani <alessvill@libero.it>
2023-01-24 17:29:38 +0100

    created Pokemon Card component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script>
 import axios from 'axios';
+import PokemonCard from './components/PokemonCard.vue';
 import { store } from './data/store'
 
 export default {
   data() {
     return {
       store
-    }
-
+    };
   },
   methods: {
     fetchPokemons() {
-      axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=10&page=1')
+      axios.get("https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=10&page=1")
         .then(res => {
-          console.log('ciao');
+          console.log("ciao");
           console.log(res.data.docs);
           store.pokemons = res.data.docs;
         }).catch(error => { }).then(() => {
-
-        })
+        });
     }
   },
-  mounted() {
+  created() {
     this.fetchPokemons();
-    console.log('ciao2');
-  }
+    console.log("ciao2");
+  },
+  components: { PokemonCard }
 }
 </script>
 
@@ -33,10 +33,7 @@ export default {
     <div class="container d-flex flex-column align-items-center py-3">
       <img class="img-fluid mb-3" src="./assets/img/pokemon-logo.png" alt="">
       <div class="pokedex row row-cols-5">
-        <div class="col d-flex flex-column align-items-center" v-for="pokemon in store.pokemons">
-          <h3>{{ pokemon.name }}</h3>
-          <img class="img-fluid" :src="pokemon.imageUrl" :alt="pokemon.name">
-        </div>
+        <PokemonCard v-for="pokemon in store.pokemons"></PokemonCard>
       </div>
     </div>
   </main>
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,22 @@
+<script>
+import { store } from '../data/store'
+
+export default {
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="col d-flex flex-column align-items-center">
+        <h3>{{ store.pokemons.name }}</h3>
+        <img class="img-fluid" :src="store.pokemons.imageUrl" :alt="store.pokemons.name">
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
