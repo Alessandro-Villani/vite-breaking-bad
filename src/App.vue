72de416bac40639306aa3d1b7c32ba9bac6bdd8a
Alessandro-Villani <alessvill@libero.it>
2023-01-25 18:37:35 +0100

    added filter - integrated with add page on scroll

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,16 @@ import axios from 'axios';
 import PokemonCard from './components/PokemonCard.vue';
 import AppLoader from './components/AppLoader.vue'
 import { store } from './data/store'
+import SelectBar from './components/generics/SelectBar.vue';
 
 export default {
   data() {
     return {
       store,
       page: 1,
-      pokemonPerPage: 10
+      pokemonPerPage: 10,
+      typeFilter: '',
+      isFiltered: false,
     };
   },
   computed: {
@@ -22,24 +25,46 @@ export default {
       store.isLoading = true;
       console.log(page);
       if (next) {
-        if (!store.hasNextPage) {
-          store.isLoading = false;
+        store.isLoading = false;
+        if (!this.isFiltered && !store.hasNextPage) {
           return
         }
         page = ++page;
         console.log(page)
       }
-      const url = `https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=${this.pokemonPerPage}&page=${page}`
-      axios.get(url)
-        .then(res => {
+      if (!this.isFiltered) {
+        console.log('is not filtered');
+        const url = `https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=${this.pokemonPerPage}&page=${page}`
+        axios.get(url)
+          .then(res => {
+            store.pokemons.push(...res.data.docs);
+            store.currentPage = res.data.page;
+            this.page = store.currentPage;
+            store.hasNextPage = res.data.hasNextPage;
+            store.totalPokemons = res.data.totalDocs;
+
+          }).catch(error => { }).then(() => {
+            store.isLoading = false;
+          });
+      } else {
+        console.log('is filtered');
+        if (page > store.filteredPages) {
+          store.isLoading = false
+          return
+        }
+        const urlFiltered = `https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?eq[type1]=${this.typeFilter}&page=${page}`
+        console.log(urlFiltered);
+        axios.get(urlFiltered).then(res => {
+          console.log(res.data);
+          console.log('pokemons ' + res.data.docs);
           store.pokemons.push(...res.data.docs);
           store.currentPage = res.data.page;
           this.page = store.currentPage;
           store.hasNextPage = res.data.hasNextPage;
-          store.totalPokemons = res.data.totalDocs;
         }).catch(error => { }).then(() => {
           store.isLoading = false;
         });
+      }
     },
     fetchPokemonTypes() {
       axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons/types1').then(res => {
@@ -50,6 +75,25 @@ export default {
       if (event.target.scrollTop + event.target.clientHeight >= event.target.scrollHeight) {
         this.fetchPokemons(store.currentPage, true);
       }
+    },
+    filterPokemons(filter) {
+      this.typeFilter = filter;
+      this.$refs.pokedex.scrollTop = 0;
+      if (filter === 'No Filter') {
+        this.isFiltered = false;
+        this.page = 1;
+        store.currentPage = 1;
+        store.pokemons = [];
+        this.fetchPokemons(this.page);
+      } else {
+        this.isFiltered = true;
+        this.page = 1;
+        store.currentPage = 1;
+        axios.get(`https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?eq[type1]=${filter}`).then(res => {
+          store.pokemons = res.data.docs
+          store.filteredPages = res.data.totalPages
+        })
+      }
     }
   },
   created() {
@@ -57,7 +101,7 @@ export default {
     this.fetchPokemonTypes();
 
   },
-  components: { PokemonCard, AppLoader }
+  components: { PokemonCard, AppLoader, SelectBar }
 }
 </script>
 
@@ -65,11 +109,11 @@ export default {
   <main>
     <div class="container d-flex flex-column align-items-center py-3">
       <img class="img-fluid mb-3" src="./assets/img/pokemon-logo.png" alt="">
-      <div v-if="pokemonNumber === store.pokemons.length || store.pokemons.length === store.totalPokemons"
-        class="pokedex row row-cols-5 p-5 rounded-2" @scroll="chargePageOnScroll">
+      <SelectBar :options="store.pokemonTypes" :default-option="'No Filter'" @option-change="filterPokemons" />
+      <div ref="pokedex" class="pokedex row row-cols-5 p-5 rounded-2" @scroll="chargePageOnScroll">
         <PokemonCard v-for="pokemon in store.pokemons" :key="pokemon._id" :id="pokemon._id"></PokemonCard>
       </div>
-      <AppLoader v-else />
+      <AppLoader v-if="store.isLoading" />
     </div>
   </main>
 </template>
--- src/components/generics/SelectBar.vue
+++ src/components/generics/SelectBar.vue
@@ -1,13 +1,22 @@
 <script>
 export default {
+    data() {
+        return {
+            optionValue: this.defaultOption || '---'
+        }
+    },
     props: {
-        options: Array
-    }
+        options: Array,
+        defaultOption: String,
+    },
+    emits: ['option-change']
 }
 </script>
 
 <template>
-    <select name="select" id="select">
+    <select class="w-50 my-3 text-center" name="select" id="select" @change="$emit('option-change', optionValue)"
+        v-model="optionValue">
+        <option>{{ defaultOption || '---' }}</option>
         <option v-for="option in options" :value="option">{{ option }}</option>
     </select>
 </template>
